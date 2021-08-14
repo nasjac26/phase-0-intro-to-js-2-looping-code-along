8c79ff9b25b66dd0b662d441555dca646f144c08
Jacob <Jacobonasrallah@gmail.com>
2021-08-14 14:34:38 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+const recipients = (['Lisa', 'Kaitlin', 'Jan'], 'surprise');
+const cardMessage = [];
+function writeCards(names, event) {
+   for (let i = 0; i < names.length; i++) {
+       
+        console.log(cardMessage.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`))
+        
+    }
+       return cardMessage; 
+}
+
+function countDown(i) {
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
+
+countDown(4);
+
+
